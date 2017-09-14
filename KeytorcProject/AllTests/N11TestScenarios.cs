da6895285727e1e094ebb59ba240f9f63eff5674
DESKTOP-CKK4JKK\hamit <hamitdogan17@gmail.com>
2017-09-14 09:43:31 +0300

    test

--- KeytorcProject/AllTests/N11TestScenarios.cs
+++ KeytorcProject/AllTests/N11TestScenarios.cs
@@ -2,15 +2,6 @@
 using KeytorcProject.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.IE;
-using OpenQA.Selenium.Interactions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace KeytorcProject.AllTests
 {
